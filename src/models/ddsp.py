86755c7baaaa4fed78e999fbd2601df6501ef818
dzjkb <jakub.pies@gmail.com>
2021-08-06 17:52:10 +0200

    bro I'm fd

---
+++ src/models/ddsp.py
@@ -0,0 +1,13 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
