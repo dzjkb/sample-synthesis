2b3209395e71ef724ed70b731174a5d68b8bdeb0
dzjkb <jakub.pies@gmail.com>
2021-11-02 21:57:27 +0100

    fix nsynth loading method

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -2,12 +2,13 @@ from os.path import join
 
 from ddsp.training import data
 
-from ..data.paths import PROCESSED
+from ..data.paths import PROCESSED, DATA_ROOT
 
 
 def get_provider(dataset, example_secs, sample_rate, frame_rate):
     if dataset == "nsynth":
         return data.NSynthTfds(
+            data_dir=join(DATA_ROOT, "nsynth", "*"),
             sample_rate=sample_rate,
             frame_rate=frame_rate,
         )
