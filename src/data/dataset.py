5e105de67839bad8f323982ca52b98dd1014a49c
dzjkb <jakub.pies@gmail.com>
2021-11-02 20:43:10 +0100

    add nsynth dataset

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -6,9 +6,15 @@ from ..data.paths import PROCESSED
 
 
 def get_provider(dataset, example_secs, sample_rate, frame_rate):
-    return data.TFRecordProvider(
-        join(PROCESSED, dataset) + "*",
-        example_secs=example_secs,
-        sample_rate=sample_rate,
-        frame_rate=frame_rate,
-    )
+    if dataset == "nsynth":
+        return data.NSynthTfds(
+            sample_rate=sample_rate,
+            frame_rate=frame_rate,
+        )
+    else:
+        return data.TFRecordProvider(
+            join(PROCESSED, dataset) + "*",
+            example_secs=example_secs,
+            sample_rate=sample_rate,
+            frame_rate=frame_rate,
+        )
