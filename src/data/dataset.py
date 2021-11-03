3cbefdc872d64ad6720e051c3ee528b228aa7586
dzjkb <jakub.pies@gmail.com>
2021-11-03 21:22:24 +0100

    custom nsynth provider

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -11,7 +11,7 @@ from ..data.paths import PROCESSED
 _AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 
-class PreprocessedTFRecordProvider(data.TFRecordProvider):
+class NSynthProvider(data.DataProvider):
     def __init__(
         self,
         file_pattern=None,
@@ -20,43 +20,61 @@ class PreprocessedTFRecordProvider(data.TFRecordProvider):
         frame_rate=250,
         preprocess_f=None,
     ):
-        super().__init__(
-            file_pattern,
-            example_secs,
-            sample_rate,
-            frame_rate,
-        )
-        self._pre_f = preprocess_f
+        self._file_pattern = file_pattern
+        self._audio_length = example_secs * sample_rate
+        self._feature_length = example_secs * frame_rate
+        super().__init__(sample_rate, frame_rate)
+        self._data_format_map_fn = tf.data.TFRecordDataset
 
     def get_dataset(self, shuffle=True):
-        ds = super().get_dataset(shuffle=shuffle)
-        if self._pre_f is not None:
-            return ds.map(self._pre_f, num_parallel_calls=_AUTOTUNE)
-        else:
-            return ds
+        def parse_tfexample(record):
+            return tf.io.parse_single_example(record, self.features_dict)
+
+        def preprocess_nsynth(ex):
+            return _nsynth_preprocess_ex(ex, self._sample_rate, self._frame_rate)
+
+        filenames = tf.data.Dataset.list_files(self._file_pattern, shuffle=shuffle)
+        dataset = filenames.interleave(
+            map_func=self._data_format_map_fn,
+            cycle_length=40,
+            num_parallel_calls=_AUTOTUNE,
+        )
+        dataset = dataset.map(parse_tfexample, num_parallel_calls=_AUTOTUNE)
+        return dataset.map(preprocess_nsynth, num_parallel_calls=_AUTOTUNE)
+
+    @property
+    def features_dict(self):
+        """NSynth features taken from https://magenta.tensorflow.org/datasets/nsynth#example-features"""
+        return {
+            'note': tf.io.FixedLenFeature([], dtype=tf.int64),
+            'note_str': tf.io.FixedLenFeature([], dtype=tf.string),
+            'instrument': tf.io.FixedLenFeature([], dtype=tf.int64),
+            'instrument_str': tf.io.FixedLenFeature([], dtype=tf.string),
+            'pitch': tf.io.FixedLenFeature([], dtype=tf.int64),
+            'velocity': tf.io.FixedLenFeature([], dtype=tf.int64),
+            'sample_rate': tf.io.FixedLenFeature([], dtype=tf.int64),
+            'audio': tf.io.FixedLenFeature([self._audio_length], dtype=tf.float32),
+            'qualities': tf.io.VarLenFeature(tf.int64),
+            'qualities_str': tf.io.VarLenFeature(tf.string),
+            'instrument_family': tf.io.FixedLenFeature([], dtype=tf.int64),
+            'instrument_family_str': tf.io.FixedLenFeature([], dtype=tf.string),
+            'instrument_source': tf.io.FixedLenFeature([], dtype=tf.int64),
+            'instrument_source_str': tf.io.FixedLenFeature([], dtype=tf.string),
+        }
 
 
 def get_provider(dataset, example_secs, sample_rate, frame_rate):
-    return PreprocessedTFRecordProvider(
+    if dataset == 'nsynth':
+        provider = NSynthProvider
+    else:
+        provider = data.TFRecordProvider
+
+    return provider(
         join(PROCESSED, dataset) + "*",
         example_secs=example_secs,
         sample_rate=sample_rate,
         frame_rate=frame_rate,
-        preprocess_f=get_preprocess_pipeline(
-            dataset,
-            example_secs,
-            sample_rate,
-            frame_rate,
-        ),
     )
-
-
-def get_preprocess_pipeline(ds_name, example_secs, sample_rate, frame_rate):
-    # if ds_name == "nsynth":
-    #     return lambda ex: _nsynth_preprocess_ex(ex, sample_rate, frame_rate)
-    # else:
-    #     return None
-    return None  # turns out nsynth examples are already preprocessed what???
 
 
 def _nsynth_preprocess_ex(ex, sample_rate, frame_rate, n_fft=2048):
@@ -69,7 +87,7 @@ def _nsynth_preprocess_ex(ex, sample_rate, frame_rate, n_fft=2048):
         #     ex['f0']['confidence'],
         'loudness_db':
             spectral_ops.compute_loudness(
-                ex['audio'],
+                ex['audio'].numpy(),
                 sample_rate,
                 frame_rate,
                 n_fft
