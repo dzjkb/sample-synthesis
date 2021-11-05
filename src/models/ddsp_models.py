38a0bee18c6dddc46a37809c9848e61c9af296f1
dzjkb <jakub.pies@gmail.com>
2021-11-05 18:24:16 +0100

    add snares-specific model

--- src/models/ddsp_models.py
+++ src/models/ddsp_models.py
@@ -228,6 +228,95 @@ def get_gauss_vae(time_steps, sample_rate, n_samples, kl_weight):
     )
 
 
+def get_snares_vae(time_steps, sample_rate, n_samples, kl_weight):
+    """
+    For the snares dataset, there is no need for the harmonic synthesizer
+    (and so f0), so we put in two noise synths instead
+    """
+
+    # parameters
+    z_dims = 32
+
+    # Create Neural Networks.
+
+    preprocessor = preprocessing.F0LoudnessPreprocessor(time_steps=time_steps)
+    encoder = encoders.MfccRnnEncoder(
+        rnn_channels=512,
+        rnn_type='gru',
+        z_dims=z_dims * 2,
+        input_keys = ('audio',),
+        mean_aggregate=True,
+    )
+    decoder = decoders.RnnFcDecoder(rnn_channels = 256,
+                                    rnn_type = 'gru',
+                                    ch = 256,
+                                    layers_per_stack = 1,
+                                    input_keys = ('ld_scaled', 'z'),
+                                    output_splits = (
+                                        # ('amps', 1),
+                                        # ('harmonic_distribution', 45),
+                                        ('noise_magnitudes_1', 45),
+                                        ('noise_magnitudes_2', 45),
+                                    ))
+
+    # Create latent distributions
+    distribution_args = {
+        'z_dims': z_dims,
+        'time_steps': time_steps,
+    }
+    posterior = GaussPosterior(**distribution_args)
+    prior = GaussPrior(**distribution_args)
+
+    # Create Processors.
+    # harmonic = ddsp.synths.Harmonic(n_samples=n_samples,
+    #                                 sample_rate=sample_rate,
+    #                                 name='harmonic')
+
+    noise1 = ddsp.synths.FilteredNoise(n_samples=n_samples,
+                                      window_size=0,
+                                      initial_bias=0,
+                                      name='noise1')
+
+    noise2 = ddsp.synths.FilteredNoise(n_samples=n_samples,
+                                      window_size=0,
+                                      initial_bias=0.0,
+                                      name='noise2')
+    add = ddsp.processors.Add(name='add')
+    # reverb = ddsp.effects.Reverb(
+    #     name='reverb',
+    #     trainable=True,
+    #     reverb_length=24000,
+    # )
+
+    # Create ProcessorGroup.
+    dag = [
+        (noise1, ['noise_magnitudes_1']),
+        (noise2, ['noise_magnitudes_2']),
+        (add, ['noise1/signal', 'noise2/signal']),
+        # (reverb, ['add/signal']),
+    ]
+
+    processor_group = ddsp.processors.ProcessorGroup(dag=dag,
+                                                     name='processor_group')
+
+    spectral_loss = ddsp.losses.SpectralLoss(
+        loss_type='L1',
+        mag_weight=1.0,
+        logmag_weight=1.0,
+    )
+    kl_loss = KLRegularizer(weight=kl_weight)
+
+    return VAE(
+        preprocessor=preprocessor,
+        encoder=encoder,
+        posterior=posterior,
+        prior=prior,
+        decoder=decoder,
+        processor_group=processor_group,
+        losses=[spectral_loss, kl_loss],
+    )
+
+
 def get_trainer(model_name, time_steps, sample_rate, n_samples, kl_weight, strategy=None, restore_checkpoint=None, **trainer_kwargs):
     if not strategy:
         strategy = strat()
@@ -247,4 +336,5 @@ def get_model(model_name, time_steps, sample_rate, n_samples, kl_weight):
         'ddsp_autoenc': get_ddsp_model,
         'iaf_vae': get_iaf_vae,
         'gauss_vae': get_gauss_vae,
+        'snares_vae': get_snares_vae,
     }[model_name](time_steps, sample_rate, n_samples, kl_weight)
